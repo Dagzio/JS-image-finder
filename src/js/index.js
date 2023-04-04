4a8b76c73e6597d4fb72cb22b140801dce8ad685
Valentine Pylypchuk <strheadshot1997@gmail.com>
2023-04-04 17:18:04 +0300

    update files

--- src/js/index.js
+++ src/js/index.js
@@ -1,45 +1,67 @@
-import  fetchImages  from "./searchImages";
+import fetchImages from "./searchImages";
+import { Notify } from "notiflix";
 
 const form = document.querySelector('#search-form');
 const formInput = form.querySelector('input[name="searchQuery"]');
 const galleryList = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
 form.addEventListener('submit', onSubmitForm);
 
 
 async function onSubmitForm(event) {
     event.preventDefault();
+    
     const name = formInput.value.trim();
     const images = await fetchImages(name);
-     galleryList.insertAdjacentElement("beforeend", await renderGalleryMarkup(images)); 
-    // form.reset();
+    const render = await renderGalleryMarkup(images);
+     galleryList.insertAdjacentHTML("beforeend", render); 
+    form.reset();
 }
 
 function renderGalleryMarkup(images) {
-    console.log(images);
-    return images.map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+
+    if (images.hits.length < 1) {
+        clearGallery();
+        loadMoreBtn.style.display = "none";
+       Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+    } else {
+        loadMoreBtn.style.display = "flex";
+    }
+    return images.hits.map(({ webformatURL, tags, likes, views, comments, downloads }) => {
         return `<div class="photo-card">
-                   <img src="${webformatURL}" alt="${Object.values(tags)}" loading="lazy" />
+                   <img class ="photo-card__image" src="${webformatURL}" alt="${tags}" loading="lazy"  />
+                
              <div class="info">
                  <p class="info-item">
-                 <b>Likes:${likes}</b>
+                 <b>Likes</b>
+                 ${likes}
 
                  </p>
                  <p class="info-item">
-                <b>Views: ${views}</b>
+                <b>Views</b>
+                ${views}
                   </p>
                   <p class="info-item">
-                 <b>Comments: ${comments}</b>
+                 <b>Comments</b>
+                 ${comments}
               </p>
                    <p class="info-item">
-              <b>Downloads: ${downloads}</b>
+              <b>Downloads</b>
+              ${downloads}
              </p>
-       </div>
-     </div>`
-    }).join('');
+             </div>
+       </div>`
+      }).join('');
 
 }; 
-// async fetchImages(name) c await на запуск функции рендера
 
-// функция/функции рендера разметки (markup)
+
+function clearGallery() {
+    galleryList.innerHTML = '';
+}
+
+// function hideLoadMoreBtn() {
+//     loadMoreBtn.style.display = "none";
+// }
 
--- src/js/searchImages.js
+++ src/js/searchImages.js
@@ -5,14 +5,15 @@ const options = {
   API_KEY: '34850720-57991e278d678856824bddd81',
   image_type: 'photo',
   orientation: 'horizontal',
-  safesearch: true
+  safesearch: true,
+  per_page: 40
 }
 
 export default async function fetchImages(name) {
-  const { BASE_URL, API_KEY, image_type, orientation, safesearch } = options;
+  const { BASE_URL, API_KEY, image_type, orientation, safesearch, per_page } = options;
   try {
-    const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${name}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}`);
-    return response;
+    const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${name}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}&per_page=${per_page}`);
+    return response.data;
   } catch (error) {
     console.error(error);
     }
